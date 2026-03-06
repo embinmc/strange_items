38a6892528e3ac15cac33fa54d9903525676781c
Embin <113154360+embinmc@users.noreply.github.com>
2026-03-06 13:59:57 -0800

    Update StrangeOptions.java

--- src/main/java/embin/strangeitems/client/StrangeOptions.java
+++ src/main/java/embin/strangeitems/client/StrangeOptions.java
@@ -8,14 +8,15 @@ import net.minecraft.client.OptionInstance;
 import net.minecraft.network.chat.Component;
 
 public class StrangeOptions {
+    static final int UNLIMITED_POINT = 51;
     private static final OptionInstance<Integer> MAP_ELEMEMT_LIMIT = new OptionInstance<>(
             "options.strangeitems.map_element_limit", OptionInstance.noTooltip(),
             (caption, value) -> {
                 String key = "options.strangeitems.map_element_limit.unlimited";
-                Component valStr = (value >= 51 ? Component.translatable(key) : Component.literal(String.valueOf(value))).withStyle(ChatFormatting.YELLOW);
+                Component valStr = (value >= UNLIMITED_POINT ? Component.translatable(key) : Component.literal(String.valueOf(value))).withStyle(ChatFormatting.YELLOW);
                 return caption.copy().append(": ").append(valStr);
             },
-            new OptionInstance.IntRange(3, 51, true), 8, num -> {}
+            new OptionInstance.IntRange(3, UNLIMITED_POINT, true), 8, num -> {}
     );
 
     public static void initalize() {
@@ -28,6 +29,6 @@ public class StrangeOptions {
 
     public static int mapElementLimit() {
         int val = StrangeOptions.MAP_ELEMEMT_LIMIT.get();
-        return val >= 51 ? 999_999 : val;
+        return val >= UNLIMITED_POINT ? 999_999 : val;
     }
 }
